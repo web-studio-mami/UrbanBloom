31da96a2fbb8776c5770ffedc1ce601828620ed1
web-studio-mami <web.studio.mami@gmail.com>
2025-11-12 08:51:37 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@
   import { createRoot } from "react-dom/client";
   import App from "./App.tsx";
   import "./index.css";
-  import "./index.html";
 
   createRoot(document.getElementById("root")!).render(<App />);
   
